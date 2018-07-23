d753981c89f15923a046eb0d8e94d9cce76e9236
MegatronusPrime <nailahharvey2003@gmail.com>
2018-07-23 23:36:38 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,2 +1,39 @@
 
 # Code your methods below
+
+def Stance
+  puts "Plant legs far apart, bend knees and keep posture loose."
+end
+def Base Foot Work
+  puts "Lift right foot."
+  puts "Return righ foot to the ground."
+  puts "Finishing with a **small skip-step** backward."
+end
+
+def Skip-Step Left
+  puts "Lower left foot to the ground"
+  puts "Balance left foot, kicking it a few inches back."
+  puts "Left, right, left and left."
+end
+
+def Skip-Step Right
+  puts "Lower right foot to the ground."
+  puts "Balance right foot< kicking it a few inches back."
+  puts "Right, left, right and right."
+end
+
+def Bob The Reins
+  puts "Holding your arms out in front of you, straight and at chest level."
+  puts "Cross your right wrist over your left abd hold them together."
+  puts "Lift your arms up and down in a loose bouncing movement in beat of the song. This movement is repeated eight times."
+end
+
+def Lasso
+  puts "Start arm movement by holding left arm so the back of your wrist is near your chin, pointing your left elbow straight left."
+  puts "Lift right arm up and point right elbow diagonally to the right."
+  puts "Raise right forearm so that it points straight up, and whip it in small circles to the beat of the song, as if you were a cowboy holding a rope lasso. This movement is repeated eight times as well."
+end
+
+def Bust
+  
+Stance
